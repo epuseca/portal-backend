0db53385c8f0dc2ab02172aa2744a954fc1f5092
epu2seca <30srecord@gmail.com>
2025-04-15 20:39:30 +0700

    Step 11: JWT auth

--- src/routes/api.js
+++ src/routes/api.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const { createUser, handleLogin, getUser } = require('../controllers/userController');
-const delay = require('../middleware/delay');
+const auth = require('../middleware/auth');
 const routerAPI = express.Router();
 
-routerAPI.all("*", delay)
+routerAPI.all("*", auth)
 
 routerAPI.get("/", (req, res) => {
     return res.status(200).json("Hello world api")
@@ -11,6 +11,7 @@ routerAPI.get("/", (req, res) => {
 
 routerAPI.post("/register", createUser)
 routerAPI.post("/login", handleLogin)
+
 routerAPI.get("/user", getUser)
 
 
