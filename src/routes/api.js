c22c101fb3644244c7269ec4f85a9a768c517e99
epu2seca <30srecord@gmail.com>
2025-04-15 16:06:36 +0700

    Step 10: lấy ra token, set timeout

--- src/routes/api.js
+++ src/routes/api.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const { createUser, handleLogin, getUser } = require('../controllers/userController');
-
+const delay = require('../middleware/delay');
 const routerAPI = express.Router();
+
+routerAPI.all("*", delay)
 
 routerAPI.get("/", (req, res) => {
     return res.status(200).json("Hello world api")
